e03252c1e7d4c5c398a1b890b36e87e7f85079ab
RichardErick <63772886+RichardErick@users.noreply.github.com>
2023-09-05 14:36:10 -0400

    Add files via upload

---
+++ TE__Cabecerass/src/main/java/com/holamundo/te__cabecerass/RespCSVRichardErickHuanapacoChura.java
@@ -0,0 +1,30 @@
+
+package com.holamundo.te__cabecerass;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RespCSV", urlPatterns = {"/RespCSV"})
+public class RespCSVRichardErickHuanapacoChura extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+       // response.setHeader("Content-Disposition","attachment;filename=archivo.xls");
+        PrintWriter out= response.getWriter();
+        try{
+        out.println("(\"empleados\":[");
+        out.println("(\"nombre\":\"Bruno\",\"apellidos\":\"Diaz\"),");
+        out.println("(\"nombre\":\"Juan jose\",\"apellidos\":\"Paredes\"),");
+        out.println("(\"nombre\":\"Dolores\",\"apellidos\":\"Fuertes\"),");
+        out.println("])");     
+        }finally{
+        out.close();
+        }
+    }
+}
